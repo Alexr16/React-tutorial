b285356069c05debadd5170fa484e43c6284b776
Alexr16 <j.alex-1996@hotmail.com>
2022-04-25 12:44:26 -0500

    Add delTodo function

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -4,11 +4,16 @@ import TodoItem from './TodoItem';
 
 class TodosList extends PureComponent {
   render() {
-    const { todos, handleChangeProps } = this.props;
+    const { todos, handleChangeProps, deleteTodoProps } = this.props;
     return (
       <ul>
         {todos.map((todo) => (
-          <TodoItem key={todo.id} todo={todo} handleChangeProps={handleChangeProps} />
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            handleChangeProps={handleChangeProps}
+            deleteTodoProps={deleteTodoProps}
+          />
         ))}
       </ul>
     );
@@ -19,6 +24,7 @@ TodosList.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
   todos: PropTypes.array.isRequired,
   handleChangeProps: PropTypes.func.isRequired,
+  deleteTodoProps: PropTypes.func.isRequired,
 };
 
 export default TodosList;
