343866e332760ab23759f783e17e18dca4217523
Alexr16 <j.alex-1996@hotmail.com>
2022-04-25 10:56:25 -0500

    Render React component

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+
+class TodoContainer extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>Hello from Create React App</h1>
+        <p>I am in a React Component!</p>
+      </div>
+    );
+  }
+}
+
+export default TodoContainer;
