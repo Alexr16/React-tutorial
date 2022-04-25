53538789d46d5ae2099399308619fc3d62a6eb61
Alexr16 <j.alex-1996@hotmail.com>
2022-04-25 12:07:12 -0500

    Refactor from class to function

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,9 @@
-import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
-class TodoItem extends PureComponent {
-  render() {
-    const { todo } = this.props;
-    return <li>{todo.title}</li>;
-  }
-}
+const TodoItem = (props) => {
+  const { todo } = props;
+  return <li>{todo.title}</li>;
+};
 
 TodoItem.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
