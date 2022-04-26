5311b37eb763100846749ba8f7c95e8565c5d36e
Alexr16 <j.alex-1996@hotmail.com>
2022-04-25 21:18:38 -0500

    Add local storage

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -8,24 +8,32 @@ class TodoContainer extends PureComponent {
   constructor() {
     super();
     this.state = {
-      todos: [
-        {
-          id: uuidv4(),
-          title: 'Setup development environment',
-          completed: true,
-        },
-        {
-          id: uuidv4(),
-          title: 'Develop website and add content',
-          completed: false,
-        },
-        {
-          id: uuidv4(),
-          title: 'Deploy to live server',
-          completed: false,
-        },
-      ],
+      todos: [],
     };
+  }
+
+  // componentDidMount() {
+  //   fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
+  //     .then((response) => response.json())
+  //     .then((data) => this.setState({ todos: data }));
+  // }
+
+  componentDidMount() {
+    const temp = localStorage.getItem('todos');
+    const loadedTodos = JSON.parse(temp);
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos,
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { todos } = this.state;
+    if (prevState.todos !== todos) {
+      const temp = JSON.stringify(todos);
+      localStorage.setItem('todos', temp);
+    }
   }
 
   handleChange = (id) => {
